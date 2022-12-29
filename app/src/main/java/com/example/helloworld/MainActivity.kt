76feba60c2034069ac56e08071f2d94006db5d75
minji05 <smj95133@likelion.org>
2022-12-29 19:08:23 +0900

    [Add]minji first commit

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -15,100 +15,29 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.example.helloworld.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    var auth: FirebaseAuth? = null
-    var googleSignInClient: GoogleSignInClient? = null
-    var GOOGLE_LOGIN_CODE = 9001
+
+    lateinit var bottom_navigation: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        FirebaseApp.initializeApp(this)
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        auth = FirebaseAuth.getInstance()
-        binding.emailLoginButton.setOnClickListener {
-            signinAndsignup()
-        }
-        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
 
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        setContentView(R.layout.activity_main)
+        bottom_navigation = findViewById(R.id.bottomNavigationView) //as BottomNavigationView
 
-        binding.googleSignInButton.setOnClickListener {
-            Log.d("test","구글 로그인 버튼 누름")
-            googleLogin()
-        }
-    }
+        // bottomNavigationView 설정
+        bottom_navigation.setOnItemSelectedListener { item->
+            when(item.itemId){
+                R.id.tab_share -> {                    val shareFragment = NutritionalFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, shareFragment).commit()
+                    return@setOnItemSelectedListener true
+                }
 
-    fun googleLogin() {
-        var signInIntent = googleSignInClient?.signInIntent
-        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == GOOGLE_LOGIN_CODE) {
-            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data!!)
-            Log.d("test","${result?.status}")
-            if (result?.isSuccess!!) {
-                var account = result.signInAccount
-                firebaseAuthWithGoogle(account)
             }
+            return@setOnItemSelectedListener false
         }
-    }
 
-    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
-        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
-        auth?.signInWithCredential(credential)?.addOnCompleteListener { it ->
-            if (it.isSuccessful) {
-                moveMainPage(it.result?.user)
-            } else {
-                Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    fun signinAndsignup() {
-        auth?.createUserWithEmailAndPassword(
-            binding.emailEditText.text.toString(),
-            binding.passwordEditText.toString()
-        )?.addOnCompleteListener { it ->
-            if (it.isSuccessful) {
-                // 성공
-                moveMainPage(it.result?.user)
-            } else if (it.exception?.message.isNullOrEmpty()) {
-                // 에러
-                Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
-            } else {
-                //
-                signInEmail()
-            }
-        }
-    }
-
-    fun signInEmail() {
-        auth?.signInWithEmailAndPassword(
-            binding.emailEditText.text.toString(),
-            binding.passwordEditText.toString()
-        )?.addOnCompleteListener { it ->
-            if (it.isSuccessful) {
-//                 아이디와 패스워드가 일치하지 않을때
-            } else {
-//                 아이디와 패스워드가 틀렸을때
-            }
-        }
-    }
-//메인으로 이동
-    fun moveMainPage(user: FirebaseUser?) {
-        if (user != null) {
-            startActivity(
-                Intent(
-                    this,
-                    MainActivity::class.java
-                ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            )
-        }
     }
 }
